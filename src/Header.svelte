a6a51ade83744b55ab7b664c34c4d36d35e5a81a
Jason Espinosa <98620802+TheGeneralisimo@users.noreply.github.com>
2022-05-10 21:54:44 -0500

    Add files via upload

---
+++ src/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<h1>PainaiApp</h1>
+  {#if $username}
+    <div class="user-bio">
+      
+      <span>Hola<strong>{$username}</strong></span>
+      <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
+    </div>
+
+    <button class="signout-button" on:click={signout}>Salir</button>
+
+    {:else}
+
+      <h3>UAEH</h3>
+  {/if}
+</header>
